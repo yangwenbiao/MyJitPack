572c9bb46c91067d9e2dae56571caae6553324d2
dev_yangwb <dev_yangwb@qq.com>
2018-01-05 17:03:36 +0800

    Initial commit

---
+++ myjitlibrary/src/main/java/com/fan/we/myjitlibrary/CutstomUtil.java
@@ -0,0 +1,15 @@
+package com.fan.we.myjitlibrary;
+
+/**
+ * des:
+ * Created by yangwb
+ * on 2018/1/5.
+ */
+
+public class CutstomUtil
+{
+    public static String getUserName()
+    {
+        return "yangwb";
+    }
+}
