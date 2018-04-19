725079f2c76dd6c8b6e4e3d070001c09ef0460ac
dev_yangwb <dev_yangwb@qq.com>
2018-04-19 11:07:19 +0800

    ceshi

--- app/src/main/java/com/fan/we/myjitpack/MainActivity.java
+++ app/src/main/java/com/fan/we/myjitpack/MainActivity.java
@@ -20,6 +20,7 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
